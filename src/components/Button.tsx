00d2267028d2e58276e9298af4f4b80c84581bb5
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-11-10 08:38:46 -0400

    feat: button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import cn from 'classnames';
+import { MouseEventHandler, ReactNode } from 'react';
+
+type Button = {
+  type: "button" | "submit" | "reset" | undefined;
+  className: string;
+  children: ReactNode;
+  onClick: MouseEventHandler | undefined;
+};
+
+
+export default function Button({type, className, children, onClick}:Button) {
+  
+  return (
+    <button
+      type={type}
+      className={cn(
+        className,
+        'transition duration-200 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5',
+      )}
+    >
+      {children}
+    </button>
+  );
+}
+
+
