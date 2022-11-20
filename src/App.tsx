318995063ea176776b3a664514c42f2d08d3a2c5
Esteban Borai <estebanborai@gmail.com>
2022-11-20 16:28:09 -0300

    feat: setup urql client with auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { authExchange } from '@urql/exchange-auth';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createClient,
+  dedupExchange,
+  cacheExchange,
+  fetchExchange,
+  Provider as UrqlProvider,
+  Operation,
+  makeOperation,
+} from 'urql';
 
 import { Index } from './views/index';
 import { LoginView } from './views/login';
@@ -19,6 +29,65 @@ const router = createBrowserRouter([
   },
 ]);
 
-export default function App() {
-  return <RouterProvider router={router} />;
+type AuthState = {
+  token?: string;
+};
+
+export default function App(): JSX.Element {
+  const urqlClient = createClient({
+    url: 'http://localhost:7878/graphql',
+    exchanges: [
+      dedupExchange,
+      cacheExchange,
+      authExchange({
+        addAuthToOperation: ({
+          authState,
+          operation,
+        }: {
+          authState: AuthState;
+          operation: Operation;
+        }) => {
+          if (!authState?.token) {
+            return operation;
+          }
+
+          const fetchOptions =
+            typeof operation.context.fetchOptions === 'function'
+              ? operation.context.fetchOptions()
+              : operation.context.fetchOptions || {};
+
+          return makeOperation(operation.kind, operation, {
+            ...operation.context,
+            fetchOptions: {
+              ...fetchOptions,
+              headers: {
+                ...fetchOptions.headers,
+                Authorization: `JWT ${authState.token}`,
+              },
+            },
+          });
+        },
+        getAuth: async ({ authState }) => {
+          if (!authState) {
+            const token = localStorage.getItem('token');
+
+            if (token) {
+              return {
+                token,
+              };
+            }
+          }
+
+          return null;
+        },
+      }),
+      fetchExchange,
+    ],
+  });
+
+  return (
+    <UrqlProvider value={urqlClient}>
+      <RouterProvider router={router} />
+    </UrqlProvider>
+  );
 }
