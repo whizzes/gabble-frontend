c70e387a37a18c40f3c15c9eaf845bc2dae54a13
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-02-13 18:58:26 -0400

    feat: button component (#12)
    Co-authored-by: Esteban Borai <estebanborai@gmail.com>

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -5,8 +5,9 @@
   import { createHeader } from '$lib/utils/basic-auth';
   import TextField from '$lib/components/TextField.svelte';
   import Card from '$lib/components/Card.svelte';
+  import Button from '$lib/components/Button.svelte';
 
-  const { handleSubmit, values, errors } = newForm({
+  const { handleSubmit, values, errors, isSubmitting } = newForm({
     initialValues: {
       email: '',
       password: ''
@@ -70,10 +71,8 @@
           error={$errors.password}
         />
 
-        <button
-          type="submit"
-          class="w-full  bg-black px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-          >Login to your account</button
+        <Button type="submit" disabled={$isSubmitting}
+          >Login to your account</Button
         >
         <div class="text-sm font-medium text-gray-500 dark:text-gray-400">
           Not registered? <a
@@ -86,50 +85,3 @@
     </Card>
   </div>
 </div>
-
-<!-- 
-<div class="flex flex-1 flex-col items-center justify-center px-10 relative">
-  <div class="flex justify-between items-center w-full py-4">
-    <div class="flex items-center justify-start space-x-3">
-      <span class="bg-black rounded-full w-6 h-6" />
-      <a href="https://whizzes.io" class="font-medium text-lg">Whizzes.io + Linx</a>
-    </div>
-    <div class="flex items-center space-x-2">
-      <span>Not a member? </span>
-      <a href="/signup" class="underline font-medium text-[#070eff]">
-        Create an account
-      </a>
-    </div>
-  </div>
-  <Card class="flex flex-1 flex-col justify-center space-y-5 max-w-md">
-    <div class="flex flex-col space-y-2 text-center">
-      <h2 class="text-3xl md:text-4xl font-bold">Sign in to account</h2>
-      <p class="text-md md:text-xl">Sign in to access all your data!</p>
-    </div>
-    <form class="flex flex-col w-full space-y-1" on:submit={handleSubmit}>
-      <TextField
-        type="text"
-        id="email"
-        name="email"
-        label="Email"
-        placeholder="E.g. user@email.com"
-        bind:value={$values.email}
-        error={$errors.email}
-      />
-      <TextField
-        type="password"
-        id="password"
-        name="password"
-        label="Password"
-        bind:value={$values.password}
-        error={$errors.password}
-      />
-      <button
-        type="submit"
-        class="flex items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black bg-black text-white"
-        >Sign in</button
-      >
-    </form>
-  </Card>
-</div>
----->
