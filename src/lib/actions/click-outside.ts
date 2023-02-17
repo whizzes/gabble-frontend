cca79c72291c53c78b97344dfc7d83fbe9f8ccb0
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-02-16 20:21:19 -0400

    fix: remove useless link and change the type of the outside click action

--- src/lib/actions/click-outside.ts
+++ src/lib/actions/click-outside.ts
@@ -1,8 +1,12 @@
 /** Dispatch event on click outside of node */
-export function clickOutside(node: any) {
+export function clickOutside(node: HTMLElement) {
   const handleClick = (event: MouseEvent) => {
-    if (node && !node.contains(event.target) && !event.defaultPrevented) {
-      node.dispatchEvent(new CustomEvent('clickOutside', node));
+    if (
+      node &&
+      !node.contains(event.target as Node) &&
+      !event.defaultPrevented
+    ) {
+      node.dispatchEvent(new CustomEvent('clickOutside'));
     }
   };
 
--- src/routes/(app)/components/Navbar/Profile.svelte
+++ src/routes/(app)/components/Navbar/Profile.svelte
@@ -41,15 +41,6 @@
           </div>
           <div class="font-medium truncate">{$page.data.user.email}!</div>
         </div>
-        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200">
-          <li>
-            <a
-              href="#"
-              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >Settings</a
-            >
-          </li>
-        </ul>
         <div class="py-2">
           <a
             href="/logout"
