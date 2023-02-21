3b651ecf484a2d7116bbcaa4af7760a1c4ed7dad
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-02-21 11:40:29 -0400

    feat: link creation

--- src/routes/(app)/components/CreateLink/CreateLink.svelte
+++ src/routes/(app)/components/CreateLink/CreateLink.svelte
@@ -1,27 +1,60 @@
 <script lang="ts">
-  import {newForm} from '@whizzes/svelte-forms'
-  import * as Yup from 'yup'
+  import { getContextClient } from '@urql/svelte';
+  import { newForm } from '@whizzes/svelte-forms';
+  import * as Yup from 'yup';
+  import { page } from '$app/stores';
 
   import Button from '$lib/components/Button.svelte';
   import Card from '$lib/components/Card.svelte';
   import TextField from '$lib/components/TextField.svelte';
-    import notification from '$lib/stores/notification';
+  import notification from '$lib/stores/notification';
+  import { LinkCreateDocument } from '$lib/graphql/schema';
 
-  const {handleSubmit, values, errors, isSubmitting} = newForm({
+  const urqlClient = getContextClient();
+
+  const { handleSubmit, values, errors, isSubmitting } = newForm({
     initialValues: {
-      url:'',
-      hash: ''
+      url: '',
+      customHash: ''
     },
     validationSchema: Yup.object({
       url: Yup.string().required(),
       hash: Yup.string()
     }),
-    onSubmit: async ({url, hash}) => {
-      notification.notifySuccess('creating link')
-      console.log("creating link")
+    onSubmit: async ({ url, customHash }) => {
+      const payload: { url: string; customHash?: string } = {
+        url
+      };
+
+      if (customHash.length) payload.customHash = customHash;
+
+      const result = await urqlClient
+        .mutation(
+          LinkCreateDocument,
+          {
+            input: payload
+          },
+          {
+            fetchOptions: {
+              headers: { authorization: `JWT ${$page.data.accessToken}` }
+            }
+          }
+        )
+        .toPromise();
+
+      if (result.error || result.data?.linkCreate?.error) {
+        return notification.notifyFailure(
+          result.data?.linkCreate?.error?.message
+        );
+      }
+
+      notification.notifySuccess(
+        `The url has been created with the hash: ${result.data?.linkCreate?.link?.id}`
+      );
     }
-  })
+  });
 </script>
+
 <Card>
   <form on:submit={handleSubmit}>
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
@@ -43,14 +76,16 @@
           id="hash"
           label="Hash"
           placeholder="example"
-          bind:value={$values.hash}
-          error={$errors.hash}
+          bind:value={$values.customHash}
+          error={$errors.customHash}
         />
       </div>
     </div>
 
     <div class="flex justify-end">
-      <Button type="submit" disabled={$isSubmitting} class="bg-blue-700">Create short!</Button>
+      <Button type="submit" disabled={$isSubmitting} class="bg-blue-700"
+        >Create short!</Button
+      >
     </div>
   </form>
 </Card>
