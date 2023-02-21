471e8e0fc240395a6c36472c17fba51b0092a901
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-02-21 11:09:47 -0400

    chore: create schema

--- src/lib/graphql/schema.ts
+++ src/lib/graphql/schema.ts
@@ -132,6 +132,28 @@ export enum UserErrorCode {
   Unknown = 'UNKNOWN'
 }
 
+export type LinkCreateMutationVariables = Exact<{
+  input: LinkCreateInput;
+}>;
+
+export type LinkCreateMutation = {
+  __typename?: 'MutationRoot';
+  linkCreate: {
+    __typename?: 'LinkCreate';
+    link?: {
+      __typename?: 'Link';
+      id: string;
+      originalUrl: string;
+      createdAt: any;
+    } | null;
+    error?: {
+      __typename?: 'LinkError';
+      code: LinkErrorCode;
+      message: string;
+    } | null;
+  };
+};
+
 export type GetCurrentUserQueryVariables = Exact<{ [key: string]: never }>;
 
 export type GetCurrentUserQuery = {
@@ -218,6 +240,21 @@ export const CurrentUserFragmentDoc = gql`
     }
     createdAt
     updatedAt
+  }
+`;
+export const LinkCreateDocument = gql`
+  mutation linkCreate($input: LinkCreateInput!) {
+    linkCreate(input: $input) {
+      link {
+        id
+        originalUrl
+        createdAt
+      }
+      error {
+        code
+        message
+      }
+    }
   }
 `;
 export const GetCurrentUserDocument = gql`
