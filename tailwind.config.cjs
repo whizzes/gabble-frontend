103500619b80a889f1fc10f1571f26e59f7f8033
Tomasz Symonowicz <tomaszsymonowicz@imac-pro-tomasz.home>
2023-02-23 19:15:43 +0100

    feat: add Inter font and set it as default

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: []
 };
