7073c3d15b7a8749495507e221ee6382587d782f
Esteban Borai <estebanborai@gmail.com>
2023-05-28 19:13:29 -0400

    chore: update dependencies and setup dependabot

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,52 +1,54 @@
 <script lang="ts">
-  import NotificationsList from '$lib/components/Notification/NotificationsList.svelte';
-
-  import {
-    dedupExchange,
-    cacheExchange,
-    fetchExchange,
-    makeOperation
-  } from '@urql/core';
+  import { cacheExchange, fetchExchange } from '@urql/core';
   import { authExchange } from '@urql/exchange-auth';
   import { createClient, setContextClient } from '@urql/svelte';
-
+  
+  import { page } from '$app/stores';
+  import NotificationsList from '$lib/components/Notification/NotificationsList.svelte';
+  
   import '../app.css';
+
+  const initializeAuthState = () => {
+    const token = $page.data?.accessToken;
+
+    return {
+      token
+    };
+  };
 
   const client = createClient({
     url: import.meta.env.VITE_GRAPHQL_URL,
     exchanges: [
-      dedupExchange,
       cacheExchange,
-      authExchange<{ accessToken?: string }>({
-        getAuth: async () => {
-          // TODO: Not implemented yet
-          return null;
-        },
-        addAuthToOperation: ({ authState, operation }) => {
-          if (!authState || !authState.accessToken) {
-            return operation;
-          }
+      authExchange(async (utils) => {
+        // https://formidable.com/open-source/urql/docs/advanced/authentication/
+        const { token } = initializeAuthState();
 
-          const fetchOptions =
-            typeof operation.context.fetchOptions === 'function'
-              ? operation.context.fetchOptions()
-              : operation.context.fetchOptions || {};
-
-          return makeOperation(operation.kind, operation, {
-            ...operation.context,
-            fetchOptions: {
-              ...fetchOptions,
-              headers: {
-                ...fetchOptions.headers,
-                Authorization: `JWT ${authState.accessToken}`
-              }
+        return {
+          addAuthToOperation(operation) {
+            if (!token) {
+              return operation;
             }
-          });
-        }
+
+            return utils.appendHeaders(operation, {
+              Authorization: `JWT ${token}`
+            });
+          },
+          async refreshAuth() {
+            window.location.href = '/logout';
+          },
+          willAuthError() {
+            return false;
+          },
+          didAuthError() {
+            return false;
+          }
+        };
       }),
       fetchExchange
     ]
   });
+
   setContextClient(client);
 </script>
 
